a363a4c80c0354908a82f21b874191905b73fa75
winter <94150047+9tailedfaux@users.noreply.github.com>
2024-01-25 13:35:11 -0600

    mechanism for recycling subgames

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,9 +2,23 @@ import kotlin.random.Random
 
 open class Game(val name: String, val maxPlayersPerTeam: Int, private val subGames: ArrayList<SubGame>? = null) {
     private var chosenSubGame: SubGame? = null
+    private val selectedSubGames = ArrayList<SubGame>()
+
+    private fun getSubGame(): SubGame? {
+        if (subGames == null) return null
+        if (subGames.isEmpty()) {
+            subGames.addAll(selectedSubGames)
+            selectedSubGames.clear()
+        }
+        return subGames.removeAt(Random.nextInt(0, subGames.size))
+            .also {
+                selectedSubGames.add(it)
+                it.onSelected()
+            }
+    }
 
     fun onSelected() {
-        chosenSubGame = subGames?.removeAt(Random.nextInt(0, subGames.size))
+        chosenSubGame = getSubGame()
     }
     override fun toString(): String {
         return if (chosenSubGame != null) "$name, $chosenSubGame"
